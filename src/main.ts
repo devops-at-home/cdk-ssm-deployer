dd2dbd2a7b4d2d516ef15815f5279e8f8897101f
DevOps@Home <devops.at.home@gmail.com>
2021-12-30 15:58:37 +0800

    add basic structure to main

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,41 @@
 import { App, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { BucketWithEventBridge } from './constructs/bucket';
+import { SSMDestination } from './constructs/destination';
+
+type Params = {
+  destinations: string[];
+};
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    // define resources here...
+    // Load params
+    const params: Params = this.node.tryGetContext('params');
+    const { destinations } = params;
+
+    // Create bucket
+    const { bucket } = new BucketWithEventBridge(this, 'Bucket');
+
+    // TODO: SSM Document for downloading package to temp folder and running script
+
+    destinations.forEach((destination) => {
+      new SSMDestination(this, `dest-${destination}`, {
+        bucket,
+        destination,
+      });
+    });
   }
 }
 
-// for development, use account/region from cdk cli
-const devEnv = {
+const env = {
   account: process.env.CDK_DEFAULT_ACCOUNT,
   region: process.env.CDK_DEFAULT_REGION,
 };
 
 const app = new App();
 
-new MyStack(app, 'my-stack-dev', { env: devEnv });
-// new MyStack(app, 'my-stack-prod', { env: prodEnv });
+new MyStack(app, 'ssm-deployer', { env });
 
 app.synth();
